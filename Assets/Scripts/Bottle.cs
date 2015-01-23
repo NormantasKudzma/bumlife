9e608db39adf7f3acdcf2dd2d8219e6f6788eb4d
ValdasPilkauskas <vpilkauskas@gmail.com>
2015-01-24 01:45:52 +0200

    Bottle ...

---
+++ Assets/Scripts/Bottle.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bottle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private void On2dCollisionEnter(Collision2D collision){
+		Player player = collision.gameObject.GetComponent<Player>();
+		player.increaseBottleCount ();
+	}
+}
