bef2308a7d76802a6b3d5145575a1db6ad3770cb
Khoa <fffirstonline@gmail.com>
2015-01-24 11:57:07 +0200

    AI2

--- Assets/Scripts/Bottle.cs
+++ Assets/Scripts/Bottle.cs
@@ -1,23 +1,27 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Bottle : MonoBehaviour {
+public class Bottle : MonoBehaviour
+{
 
-	public int AmountOfStench = 10;
-	// Use this for initialization
-	void Start () {
+		public int AmountOfStench = 10;
+		// Use this for initialization
+		void Start ()
+		{
 	
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	}
+		}
 
-	private void OnCollisionEnter(Collision collision){
-		Bum bum = collision.gameObject.GetComponent<Bum>();
-		bum.increaseBottleCount ();
-		bum.addStenchRadius (this.AmountOfStench);
-		Destroy (GetComponent<Bottle>());
-	}
+		private void OnCollisionEnter (Collision collision)
+		{
+				Bum bum = collision.gameObject.GetComponent<Bum> ();
+				bum.increaseBottleCount ();
+				bum.addStenchRadius (this.AmountOfStench);
+				Destroy (GetComponent<Bottle> ());
+		}
 }
