587bb5d96a9c7284e658a1ec90b3ce5f0b99cc17
NormantasKudzma <kudzma.normantas@gmail.com>
2015-01-24 10:38:35 +0200

    Player rotation, following basics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,23 @@ public class PlayerController : MonoBehaviour {
 	private Vector3 moveStartPos;
 	private float moveStartTime;
 	private float moveDistance;
-	private bool isMoving = false;
+	public bool isMoving = false;
 	private float movementSpeed = 6.5f;
+	
+	public Pedestrian movementTarget;
+	public bool isFollowing = false;
 	
 	void Update(){
 		if (Input.GetMouseButtonDown(0)){
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			movementDestination = ray.origin;
-			movementDestination.z = transform.position.z;
-			moveStartTime = Time.time;
-			moveStartPos = transform.position;
-			moveDistance = Vector3.Distance(moveStartPos, movementDestination);
+			setMovementDestination(ray.origin);
 			isMoving = true;
+			isFollowing = false;
 		}
 		if (isMoving){
+			if (isFollowing){
+				followTarget(movementTarget);
+			}
 			movePlayer();
 		}
 	}
@@ -33,11 +36,35 @@ public class PlayerController : MonoBehaviour {
 		}
 	}
 	
+	void rotatePlayer(Vector3 point){
+		transform.LookAt(point, transform.up);
+	}
+	
 	void OnCollisionEnter(Collision col){
 		stopMoving();
 	}
 	
 	public void stopMoving(){
 		isMoving = false;
+		isFollowing = false;
+	}
+	
+	public void startFollowing(Pedestrian target){
+		movementTarget = target;
+		isFollowing = true;
+		isMoving = true;
+	}
+	
+	void followTarget(Pedestrian target){
+		setMovementDestination(target.transform.position);
+	}
+	
+	void setMovementDestination(Vector3 pos){
+		movementDestination = pos;
+		movementDestination.z = transform.position.z;
+		moveStartTime = Time.time;
+		moveStartPos = transform.position;
+		moveDistance = Vector3.Distance(moveStartPos, movementDestination);
+		rotatePlayer(movementDestination);
 	}
 }
