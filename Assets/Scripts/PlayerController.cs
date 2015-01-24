622bc4d1bd67a484ac143877b728704fd0a8d87b
NormantasKudzma <kudzma.normantas@gmail.com>
2015-01-24 09:47:16 +0200

    Added player movement (functional)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,43 @@
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	private Vector3 movementDirection;
-	private float maxSpeed = 6.5f;
+	private Vector3 movementDestination;
+	private Vector3 moveStartPos;
+	private float moveStartTime;
+	private float moveDistance;
+	private bool isMoving = false;
+	private float movementSpeed = 6.5f;
 	
 	void Update(){
-		for (int i = 0; i < Input.touchCount; i++){
-			Touch t = Input.GetTouch(i);
+		if (Input.GetMouseButtonDown(0)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			movementDestination = ray.origin;
+			movementDestination.z = transform.position.z;
+			moveStartTime = Time.time;
+			moveStartPos = transform.position;
+			moveDistance = Vector3.Distance(moveStartPos, movementDestination);
+			isMoving = true;
 		}
+		if (isMoving){
+			movePlayer();
+		}
+	}
+	
+	void movePlayer(){
+		float distCovered = (Time.time - moveStartTime) * movementSpeed;
+		float fracJourney = distCovered / moveDistance;
+		transform.position = Vector3.Lerp(moveStartPos, movementDestination, fracJourney);
+		
+		if (fracJourney >= 1){
+			stopMoving();
+		}
+	}
+	
+	void OnCollisionEnter(Collision col){
+		stopMoving();
+	}
+	
+	public void stopMoving(){
+		isMoving = false;
 	}
 }
